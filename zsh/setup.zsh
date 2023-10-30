3c607f657070266443074f09361e979cf7cf15df
weirdgiraffe <giraffe@cyberzoo.xyz>
2023-10-30 15:12:04 +0100

    cleaned up config a bit

--- zsh/setup.zsh
+++ zsh/setup.zsh
@@ -1,14 +1,28 @@
+rm -rf ~/.zim
+rm -rf ~/.zimrc
+
+cat << \EOF >| ~/.zshrc
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+EOF
+
 if [[ $(uname) == "Darwin" ]]; then
-    rm -rf ~/.zim
-    rm -rf ~/.zimrc
-    rm -rf ~/.zshrc
     source os/mac/packages.zsh
     source packages.zsh
-    cp p10k.zsh ~/.p10k.zsh
-    cat env.zsh >> ~/.zshrc
-    cat os/mac/aliases.zsh >> ~/.zshrc
-    cat aliases.zsh >> ~/.zshrc
-    echo >> ~/.zshrc
-    echo "[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh" >> ~/.zshrc
 fi
+
+cp p10k.zsh ~/.p10k.zsh
+cat env.zsh >> ~/.zshrc
+cat os/mac/aliases.zsh >> ~/.zshrc
+cat aliases.zsh >> ~/.zshrc
+
+cat << \EOF >> ~/.zshrc
+
+[[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh"
+EOF
 
