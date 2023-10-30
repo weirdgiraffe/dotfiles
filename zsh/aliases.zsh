632aa84ad7984072cf6881c08a338cbf3fe57a06
weirdgiraffe <giraffe@cyberzoo.xyz>
2023-10-30 13:56:10 +0100

    add functions for date

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,31 @@
-alias   vi="nvim"
-alias  vim="nvim"
+alias vi="nvim"
+alias vim="nvim"
+alias tf='terraform'
+alias k='kubectl'
+alias dc="docker compose"
+
+alias myip='curl -s http://ip-api.com/json| python -m json.tool'
+alias urldecode='python -c "import sys, urllib as ul; print ul.unquote_plus(sys.argv[1])"'
+alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
+
+alias rfcdate='date -u +"%Y-%m-%dT%H:%M:%SZ"'
+alias unixdate='date +"%s"'
+
+rfcdate() {
+  local date=$(command -v gdate)
+  local input=$1
+  if [ -z ${input} ];then
+    TZ=UTC ${gdate:=date} --date='@'${input}
+  else 
+    TZ=UTC ${gdate:=date}
+  fi
+}
+
+unixddate() {
+  local input=$1
+  if [ -z ${input} ];then
+    ${gdate:=date} --date=${input} +%s
+  else 
+    ${gdate:=date} +%s
+  fi
+}
