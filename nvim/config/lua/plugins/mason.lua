8fdd845872eba2ebc7c9044aefcee203ba8bf9ec
weirdgiraffe <giraffe@cyberzoo.xyz>
2023-11-18 22:35:28 +0100

    fixup mason server configuration
    - apply the basic configuration for every server
    - then configure each server individually
    - install utilities in a better fasion

--- nvim/config/lua/plugins/mason.lua
+++ nvim/config/lua/plugins/mason.lua
@@ -1,11 +1,28 @@
-local function with_default_options(default_opts)
+local function with_defaults(defaults)
+  defaults = defaults or function() return {} end
+  local configs = {}
   return {
-    setup = function(server_name, opts)
-      opts = vim.tbl_extend("force", default_opts, opts or {})
-      local msg = string.format("[%s].opts=%s\n", server_name, vim.inspect(opts))
-      vim.print(msg)
-      local lspconfig = require("lspconfig")
-      lspconfig[server_name].setup(opts)
+    extend = function(server_configs)
+      if type(server_configs) == "table" then
+        for server_name, config in pairs(server_configs) do
+          if type(config) == "function" then
+            configs[server_name] = config
+          else
+            vim.notify(string.format(
+                "[%s]: invalid config (must be a function): %s\n",
+                server_name,
+                vim.inspect(config)),
+              vim.log.levels.WARN,
+              {})
+          end
+        end
+      end
+    end,
+    setup_server = function(name)
+      local default_opts = defaults() or {}
+      local server_opts = configs[name] and configs[name]() or {}
+      local opts = vim.tbl_extend("force", default_opts, server_opts)
+      require("lspconfig")[name].setup(opts)
     end
   }
 end
@@ -15,6 +32,7 @@ return {
   "williamboman/mason.nvim",
   lazy = false,
   build = function()
+    ---@diagnostic disable-next-line: param-type-mismatch
     pcall(vim.cmd, "MasonUpdate")
   end,
   dependencies = {
@@ -24,8 +42,40 @@ return {
   },
   config = function()
     require("mason").setup()
-    local lspconfig = require("lspconfig")
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    local lspconfig = with_defaults(function()
+      return {
+        on_attach = require('user.lsp').on_attach,
+        capabilities = require('cmp_nvim_lsp').default_capabilities()
+      }
+    end)
+    lspconfig.extend({
+      ["lua_ls"] = function()
+        require("neodev").setup()
+        return {
+          on_attach = require('user.lsp').on_attach,
+          settings = {
+            Lua = {
+              workspace = {
+                -- diable annoying message about lua environment
+                checkThirdParty = false,
+              },
+            },
+          },
+        }
+      end,
+      ["rust_analyzer"] = function()
+        return {
+          on_attach = require('user.lsp').on_attach,
+          settings = {
+            ["rust-analyzer"] = {
+              checkOnSave = {
+                command = "clippy-driver",
+              },
+            }
+          }
+        }
+      end
+    })
     require("mason-lspconfig").setup({
       ensure_installed = {
         "lua_ls",
@@ -37,43 +87,16 @@ return {
         -- The first entry (without a key) will be the default handler
         -- and will be called for each installed server that doesn't have
         -- a dedicated handler.
-        function(server_name) -- default handler (optional)
-          lspconfig[server_name].setup({
-            on_attach = require('user.lsp').on_attach,
-            capabilities = capabilities,
-          })
-        end,
-        ["lua_ls"] = function()
-          require("neodev").setup()
-          lspconfig.lua_ls.setup({
-            on_attach = require('user.lsp').on_attach,
-            settings = {
-              Lua = {
-                workspace = {
-                  -- diable annoying message about lua environment
-                  checkThirdParty = false,
-                },
-              },
-            },
-          })
-        end,
-        ["rust_analyzer"] = function()
-          lspconfig.rust_analyzer.setup({
-            on_attach = require('user.lsp').on_attach,
-            settings = {
-              ["rust-analyzer"] = {
-                checkOnSave = {
-                  command = "clippy-driver",
-                },
-              }
-            }
-          })
-        end
+        lspconfig.setup_server
       },
     })
-    require("mason.api.command").MasonInstall({
-      "yamlfmt",
-      --"rustfmt",
-    })
+
+    local to_install = {}
+    if vim.fn.executable('yamlfmt') == 0 then
+      table.insert(to_install, "yamlfmt")
+    end
+    if #to_install > 0 then
+      require("mason.api.command").MasonInstall(to_install)
+    end
   end,
 }
