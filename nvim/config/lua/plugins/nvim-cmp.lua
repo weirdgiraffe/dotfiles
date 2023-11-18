3d1ad79bcaaf8b54bf43b50219af65639d008b25
weirdgiraffe <giraffe@cyberzoo.xyz>
2023-11-18 22:35:54 +0100

    try to play with nvim-cmp configuration

--- nvim/config/lua/plugins/nvim-cmp.lua
+++ nvim/config/lua/plugins/nvim-cmp.lua
@@ -1,6 +1,70 @@
+local function lspkind_formatting()
+  return {
+    format = require("lspkind").cmp_format({
+      maxwidth = 50,
+      ellipsis_char = "...",
+    }),
+  }
+end
+
+-- another interesting issue: https://github.com/hrsh7th/nvim-cmp/issues/771
+-- https://github.com/sbernheim4/dotfiles/blob/master/vim/lua/cmp_settings.lua
+
+-- https://github.com/hrsh7th/nvim-cmp/discussions/609#discussioncomment-5727678
+local function custom_formatting()
+  return {
+    fields = { "abbr", "menu", "kind" },
+    format = function(entry, item)
+      -- Define menu shorthand for different completion sources.
+      local menu_icon = {
+        nvim_lsp = "NLSP",
+        nvim_lua = "NLUA",
+        luasnip  = "LSNP",
+        buffer   = "BUFF",
+        path     = "PATH",
+      }
+      -- Set the menu "icon" to the shorthand for each completion source.
+      item.menu = menu_icon[entry.source.name]
+
+      -- Set the fixed width of the completion menu to 60 characters.
+
+      -- Set 'fixed_width' to false if not provided.
+      local fixed_width = fixed_width or false
+
+      -- Get the completion entry text shown in the completion window.
+      local content = item.abbr
+
+      -- Set the fixed completion window width.
+      if fixed_width then
+        vim.o.pumwidth = fixed_width
+      end
+
+      -- Get the width of the current window.
+      local win_width = vim.api.nvim_win_get_width(0)
+
+      -- Set the max content width based on either: 'fixed_width'
+      -- or a percentage of the window width, in this case 20%.
+      -- We subtract 10 from 'fixed_width' to leave room for 'kind' fields.
+      local max_content_width = fixed_width and fixed_width - 10 or math.floor(win_width * 0.2)
+
+      -- Truncate the completion entry text if it's longer than the
+      -- max content width. We subtract 3 from the max content width
+      -- to account for the "..." that will be appended to it.
+      if #content > max_content_width then
+        item.abbr = vim.fn.strcharpart(content, 0, max_content_width - 3) .. "..."
+      else
+        item.abbr = content .. (" "):rep(max_content_width - #content)
+      end
+      return item
+    end,
+  }
+end
+
+
+
 return {
   'hrsh7th/nvim-cmp',
-  event = "BufWinEnter",
+  event = "InsertEnter",
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/vim-vsnip",
@@ -13,14 +77,17 @@ return {
     local cmp = require('cmp')
 
     cmp.setup({
+      completion = {
+        completeopt = "menu,menuone,preview,noselect",
+      },
       snippet = {
         expand = function(args)
           vim.fn["vsnip#anonymous"](args.body)
         end,
       },
       window = {
-        -- completion = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
       },
       mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -32,12 +99,12 @@ return {
       sources = cmp.config.sources({
         { name = 'nvim_lsp' },
         { name = 'vsnip' }, -- For vsnip users.
+        { name = 'buffer' },
         -- { name = 'luasnip' }, -- For luasnip users.
         -- { name = 'ultisnips' }, -- For ultisnips users.
         -- { name = 'snippy' }, -- For snippy users.
-      }, {
-        { name = 'buffer' },
-      })
+      }),
+      formatting = custom_formatting(),
     })
   end
 }
