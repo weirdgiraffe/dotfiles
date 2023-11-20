1b43f4851f54352923bdc6f71f1c301e245868fe
weirdgiraffe <giraffe@cyberzoo.xyz>
2023-11-20 09:39:04 +0100

    show relative name istead of filename in the line

--- nvim/config/lua/plugins/laline.lua
+++ nvim/config/lua/plugins/laline.lua
@@ -22,5 +22,24 @@ return {
       lualine_y = {},
       lualine_z = {},
     },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = { 'branch', 'diff', 'diagnostics' },
+      lualine_c = {
+        {
+          'filename',
+          path = 1, --[[ 0: Just the filename
+                         1: Relative path
+                         2: Absolute path
+                         3: Absolute path, with tilde as the home directory
+                         4: Filename and parent dir, with tilde as the home
+                            directory
+                      ]] --
+        },
+      },
+      lualine_x = { 'encoding', 'fileformat', 'filetype' },
+      lualine_y = { 'progress' },
+      lualine_z = { 'location' }
+    },
   },
 }
