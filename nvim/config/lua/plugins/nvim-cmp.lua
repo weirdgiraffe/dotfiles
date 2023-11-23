b12826a2c2df5aaa954386d08425dd24f7fbff31
weirdgiraffe <giraffe@cyberzoo.xyz>
2023-11-23 15:14:56 +0100

    fixup: completion configuration

--- nvim/config/lua/plugins/nvim-cmp.lua
+++ nvim/config/lua/plugins/nvim-cmp.lua
@@ -4,8 +4,6 @@ local function has_words_before()
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
-
-
 -- limited width completion formatting inspired by
 -- https://github.com/hrsh7th/nvim-cmp/discussions/609#discussioncomment-5727678
 local function make_formatting_function()
@@ -32,15 +30,14 @@ local function make_formatting_function()
   end
 end
 
-
-
-
 return {
   'hrsh7th/nvim-cmp',
-  event = "InsertEnter",
+  event = "BufWinEnter",
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
+    "FelipeLema/cmp-async-path",
+    "hrsh7th/cmp-cmdline",
     "onsails/lspkind.nvim",
     "hrsh7th/cmp-nvim-lsp-signature-help",
     "L3MON4D3/LuaSnip",
@@ -104,11 +101,14 @@ return {
       }),
       sources = cmp.config.sources({
         { name = 'nvim_lsp' },
-        { name = 'buffer' },
         { name = 'luasnip' },
+        { name = 'async_path' },
         -- { name = 'ultisnips' }, -- For ultisnips users.
         -- { name = 'snippy' }, -- For snippy users.
+      }, {
+        { name = 'buffer' },
       }),
+
       formatting = {
         fields = { "kind", "abbr", "menu" },
         format = make_formatting_function(),
