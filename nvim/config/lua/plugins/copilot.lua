df29dff623c5a36092a4a98f4717c9ce73b4d7a7
weirdgiraffe <giraffe@cyberzoo.xyz>
2023-11-23 15:17:26 +0100

    fixup: keybindings for copilot

--- nvim/config/lua/plugins/copilot.lua
+++ nvim/config/lua/plugins/copilot.lua
@@ -1,7 +1,7 @@
 return {
   "zbirenbaum/copilot.lua",
   -- pure lua replacement of github/copilot.vim
-  event = "InsertEnter",   -- check available events: https://neovim.io/doc/user/autocmd.html#autocmd-events
+  event = "InsertEnter", -- check available events: https://neovim.io/doc/user/autocmd.html#autocmd-events
   config = function()
     require("copilot").setup({
       suggestion = {
@@ -9,12 +9,12 @@ return {
         auto_trigger = true,
         debounce = 75,
         keymap = {
-          accept = "<M-l>",
+          accept = "<C-l>",
           accept_word = false,
           accept_line = false,
-          next = "<M-j>",
-          prev = "<M-k>",
-          dismiss = "<M-h>",
+          next = "<C-j>",
+          prev = "<C-k>",
+          dismiss = "<C-h>",
         },
       },
     })
