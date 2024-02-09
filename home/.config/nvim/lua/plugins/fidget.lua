8666e684ad6f6d00319f869e8eecd2d6f4372cff
weirdgiraffe <giraffe@cyberzoo.xyz>
2024-02-09 09:41:18 +0100

    nvim: make transparent fidget

--- home/.config/nvim/lua/plugins/fidget.lua
+++ home/.config/nvim/lua/plugins/fidget.lua
@@ -1,5 +1,6 @@
 return {
   "j-hui/fidget.nvim",
+  version = "1.3.0",
   event = "LspAttach",
   opts = {
     progress = {
@@ -9,6 +10,10 @@ return {
           period = 1,
         },
       },
+    },
+    notification = {
+      -- make background of popup notifications transparent
+      window = { winblend = 0, },
     },
   },
 }
