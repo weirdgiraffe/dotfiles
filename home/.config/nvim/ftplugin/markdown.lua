eb626d792779eec9e29a32a961af88140f4fb9ca
weirdgiraffe <giraffe@cyberzoo.xyz>
2024-02-10 22:09:04 +0100

    nvim: use inactive background color for markdown color column

--- home/.config/nvim/ftplugin/markdown.lua
+++ home/.config/nvim/ftplugin/markdown.lua
@@ -1,7 +1,5 @@
 vim.o.textwidth = 80
 vim.o.colorcolumn = "+1"
--- set color of the 80th column
-vim.api.nvim_set_hl(0, "ColorColumn", { bg = 'lightgrey' })
 
 vim.o.tabstop = 8
 vim.o.softtabstop = 2
