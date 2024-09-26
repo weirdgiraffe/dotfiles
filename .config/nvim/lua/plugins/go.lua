d6acbfab80d8e82d483264fdd991c7c87e135e26
weirdgiraffe <giraffe@cyberzoo.xyz>
2024-09-26 14:18:41 +0200

    add workaround for go workspaces

--- .config/nvim/lua/plugins/go.lua
+++ .config/nvim/lua/plugins/go.lua
@@ -10,12 +10,12 @@ return {
   build = function() require("go.install").install_all_sync() end,
   opts = {
     log_path = vim.fn.stdpath('cache') .. "/go.nvim.log", -- this plugin has a weird default log path, so fix it
-    lsp_cfg = true,                                       -- use go.nvim specific convig for gopls
+    lsp_cfg = false,                                      -- use go.nvim specific convig for gopls
     lsp_keymaps = false,                                  -- disable default lsp keymaps
     lsp_inlay_hints = {
       enable = false,                                     -- inlay hints are always toggling on save, so it's quite annoying
     },
-    tag_transform = "snakecase",                          -- check -transform of gomodifytags
+    tag_transform = "camelcase",                          -- check -transform of gomodifytags
     tag_options = 'json=omitempty',                       -- check -add-options of gomodifytags
     comment_placeholder = "",                             -- placeholder for require("go.comment").gen()
     trouble = true,                                       -- true: use trouble to open quickfix
