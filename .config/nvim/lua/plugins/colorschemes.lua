4d8e9229d5c18f3ba1b4f7fbbf4066804b168d74
weirdgiraffe <giraffe@cyberzoo.xyz>
2024-10-04 11:30:08 +0200

    theme: a bit more adjustments for everforest

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -48,6 +48,8 @@ return {
     'sainnhe/everforest',
     priority = 999,
     config = function()
+      vim.g.everforest_background = "hard"
+      vim.g.everforest_better_performance = 1
       vim.cmd([[colorscheme everforest]])
     end,
   }
