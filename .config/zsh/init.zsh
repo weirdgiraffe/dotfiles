fc4196a8e0d23cc3eda7f41ff52b1d69d8f625bf
weirdgiraffe <giraffe@cyberzoo.xyz>
2024-10-10 00:55:51 +0200

    add: more work related settings

--- .config/zsh/init.zsh
+++ .config/zsh/init.zsh
@@ -1,6 +1,6 @@
 CURRENT_SCRIPT_DIR=${${(%):-%x}:a:h}
 
-steps=(paths zim oh-my-posh fzf history zoxide other)
+steps=(paths zim oh-my-posh fzf history zoxide other work)
 for ((i = 1; i <= $#steps; i++)); do
     local script="${CURRENT_SCRIPT_DIR}/${steps[i]}.zsh"
     [[ -r "${script}" ]] && source "${script}"
