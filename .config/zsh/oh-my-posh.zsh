1a9db8755fc9b7d1670121a76c3ea5a025658995
weirdgiraffe <giraffe@cyberzoo.xyz>
2024-10-26 00:57:11 +0200

    fixup: slow shell start because of pyenv

--- .config/zsh/oh-my-posh.zsh
+++ .config/zsh/oh-my-posh.zsh
@@ -1,2 +1,5 @@
-eval "$(oh-my-posh init zsh --config=${HOME}/.config/oh-my-posh/config.toml)"
+if [[ ! -f ${HOME}/.config/zsh/oh-my-posh-init.zsh ]]; then 
+  oh-my-posh init zsh --config=${HOME}/.config/oh-my-posh/config.toml > ${HOME}/.config/zsh/oh-my-posh-init.zsh
+fi
+source ${HOME}/.config/zsh/oh-my-posh-init.zsh
 
