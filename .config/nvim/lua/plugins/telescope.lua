1ba3c0e59a5f68440a202636a2af7655054da90b
weirdgiraffe <giraffe@cyberzoo.xyz>
2024-10-27 09:47:03 +0100

    wip: experiments with telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,23 +6,14 @@ return {
     'nvim-lua/plenary.nvim',
   },
   config = function()
-    local actions = require "telescope.actions"
     local telescope = require("telescope")
     telescope.setup({
       defaults = {
         path_display = { "smart" },
-        mappings = {
-          i = {
-            ["<C-j>"] = actions.move_selection_next,
-            ["<C-n>"] = actions.move_selection_next,
-            ["<C-k>"] = actions.move_selection_previous,
-            ["<C-p>"] = actions.move_selection_previous,
-          }
-        }
       },
       extensions = {
         ["ui-select"] = {
-          require("telescope.themes").get_dropdown()
+          require("telescope.themes").get_ivy()
         },
       }
     })
