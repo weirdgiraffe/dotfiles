86b96d2530fad39557b4def0971205591e9206f3
weirdgiraffe <giraffe@cyberzoo.xyz>
2024-10-30 12:12:16 +0100

    final version of experiment

---
+++ zsh-experiment/.cache/zsh/functions/pyenv-virtualenv.zsh
@@ -0,0 +1,15 @@
+export PATH="/opt/homebrew/Cellar/pyenv-virtualenv/1.2.4/shims:${PATH}";
+export PYENV_VIRTUALENV_INIT=1;
+_pyenv_virtualenv_hook() {
+  local ret=$?
+  if [ -n "${VIRTUAL_ENV-}" ]; then
+    eval "$(pyenv sh-activate --quiet || pyenv sh-deactivate --quiet || true)" || true
+  else
+    eval "$(pyenv sh-activate --quiet || true)" || true
+  fi
+  return $ret
+};
+typeset -g -a precmd_functions
+if [[ -z $precmd_functions[(r)_pyenv_virtualenv_hook] ]]; then
+  precmd_functions=(_pyenv_virtualenv_hook $precmd_functions);
+fi
---
+++ zsh-experiment/.config/zsh/functions/work.zsh
@@ -0,0 +1,1 @@
+export GOPRIVATE=${GOPRIVATE},github.com/chronicleprotocol
