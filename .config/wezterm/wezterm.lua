c74012e54c6019e6a5ace5b6bd5090c46e62a16e
weirdgiraffe <giraffe@cyberzoo.xyz>
2024-11-06 09:21:07 +0100

    fixup: wezterm config to not overload GPU on zoom

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -15,20 +15,29 @@ local function scheme_for_appearance(appearance)
     return "Everforest Light (Gogh)"
   end
 end
-config.color_scheme = scheme_for_appearance(wezterm.gui.get_appearance())
+config.color_scheme                               = scheme_for_appearance(wezterm.gui.get_appearance())
 
-config.window_padding = {
+config.window_padding                             = {
   left = 20,
   right = 20,
   top = 10,
   bottom = 10,
 }
 
-config.enable_tab_bar = false
-config.window_decorations = 'RESIZE'
+config.window_decorations                         = "TITLE | RESIZE"
+-- config.enable_tab_bar = false
+config.hide_tab_bar_if_only_one_tab               = true
+config.max_fps                                    = 120
 
 config.adjust_window_size_when_changing_font_size = false
-config.automatically_reload_config = true
+config.hide_mouse_cursor_when_typing              = true
+config.exit_behavior                              = "Close"
+
+-- configure opacity and gpu
+config.front_end                                  = "WebGpu"
+config.window_background_opacity                  = 0.99
+config.macos_window_background_blur               = 50
+
 
 -- and finally, return the configuration to wezterm
 return config
