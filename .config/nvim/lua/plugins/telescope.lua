bd4e3085a203713ca0786b1ecbec34c41c819631
weirdgiraffe <giraffe@cyberzoo.xyz>
2024-11-13 14:37:48 +0100

    nvim: better view for telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,3 +1,32 @@
+local Path = require("plenary.path")
+local utils = require("utils")
+
+local function path_filename_first(path)
+  local sep = Path.path.sep
+  local dirs = vim.split(path, sep)
+  local filename = dirs[#dirs]
+
+  -- typical file path which I expect is something like:
+  --  - .../github.com/username/...
+  --  - .../gitlab.com/username/...
+  -- so I will just start from it
+  for i = 1, #dirs do
+    if dirs[i] == "github.com" or dirs[i] == "gitlab.com" then
+      for j = 1, i do
+        table.remove(dirs, 1)
+      end
+    end
+  end
+
+  local tail = table.concat(dirs, sep)
+  -- Trim prevents a top-level filename to have a trailing white space
+  local transformed_path = vim.trim(filename .. " " .. tail)
+  local path_style = { { { #filename, #transformed_path }, "TelescopeResultsComment" } }
+  return transformed_path, path_style
+end
+
+
+
 return {
   "nvim-telescope/telescope.nvim",
   tag = '0.1.8',
@@ -11,7 +40,9 @@ return {
     local actions = require("telescope.actions")
     telescope.setup({
       defaults = {
-        path_display = { "smart" },
+        path_display = function(opts, path)
+          return path_filename_first(path)
+        end,
         mappings = {
           i = {
             ["<C-j>"] = actions.move_selection_next,
