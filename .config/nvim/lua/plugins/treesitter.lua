1532fef3183273b1ff7003fc5854e6cfff716a53
weirdgiraffe <giraffe@cyberzoo.xyz>
2024-11-13 14:34:39 +0100

    fixup: exclude dockerfile from treesitter for a better syntax support

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -31,6 +31,7 @@ return {
         -- Using this option may slow down your editor, and you may see some duplicate highlights.
         -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting = { "markdown" },
+        disable = { "dockerfile" },
       },
       indent = {
         enable = true,
