c3ee2d3d7d9911c6525b133a409fcacc02eb601b
weirdgiraffe <giraffe@cyberzoo.xyz>
2024-11-13 16:01:20 +0100

    nvim: make completions work again

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -4,42 +4,24 @@
 --  https://github.com/linkarzu/dotfiles-latest/blob/main/neovim/neobean/lua/plugins/nvim-cmp.lua
 --  https://github.com/omerxx/dotfiles/blob/master/nvim/lua/plugins/cmp.lua
 --  https://github.com/tjdevries/config.nvim/blob/master/lua/custom/completion.lua
+--  https://github.com/Roundlay/nvim/blob/main/lua/plugins/lazy-cmp.lua
 --
-
-local function has_words_before()
-  unpack = unpack or table.unpack
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
 
 -- limited width completion formatting inspired by
 -- https://github.com/hrsh7th/nvim-cmp/discussions/609#discussioncomment-5727678
-local function make_formatting_function()
-  local lspkind_format = require("lspkind").cmp_format({
-    maxwidth = 50,
+local function make_format_function()
+  local lspkind = require("lspkind")
+  local impl = lspkind.cmp_format({
+    mode = "symbol_text", -- show only symbol annotations
+    maxwidth = {
+      menu = 50,
+      abbr = 50,
+    },
     ellipsis_char = "...",
+    show_labelDetails = true,
   })
-
-  local floors = {}
   return function(entry, item)
-    -- Get the width of the current window.
-    local win_width = vim.api.nvim_win_get_width(0)
-    -- Set the max content to a percentage of the window width, in this case 20%.
-    local max_content_width = floors[win_width]
-    if not max_content_width then
-      max_content_width = math.floor(win_width * 0.2)
-      floors[win_width] = max_content_width
-    end
-    -- Truncate the completion entry text if it's longer than the
-    -- max content width. We subtract 3 from the max content width
-    -- to account for the "..." that will be appended to it.
-    local content = item.abbr
-    if #content > max_content_width then
-      item.abbr = vim.fn.strcharpart(content, 0, max_content_width - 3) .. "..."
-    else
-      item.abbr = content .. (" "):rep(max_content_width - #content)
-    end
-    return lspkind_format(entry, item)
+    return impl(entry, item)
   end
 end
 
@@ -48,106 +30,159 @@ return {
   event = "BufWinEnter",
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-path",
     "hrsh7th/cmp-buffer",
-    "FelipeLema/cmp-async-path",
-    "hrsh7th/cmp-cmdline",
-    "onsails/lspkind.nvim",
     "hrsh7th/cmp-nvim-lsp-signature-help",
+    "onsails/lspkind.nvim",
     "L3MON4D3/LuaSnip",
     "saadparwaiz1/cmp_luasnip"
   },
   lazy = false,
   config = function()
     local cmp = require('cmp')
-    local luasnip = require('luasnip')
 
     local actions = {
-      --- select next should select next completion item of jump to the next snippet parameter
-      select_next = cmp.mapping(function(fallback)
+      complete_or_select_next_item = cmp.mapping(function(fallback)
         if cmp.visible() then
+          vim.notify("select_next: cmp visible: select_next_item")
           cmp.select_next_item()
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        elseif vim.api.nvim_buf_get_option(0, 'buftype') ~= 'prompt' and has_words_before() then
+        elseif vim.api.nvim_buf_get_option(0, 'buftype') ~= 'prompt' then
+          vim.notify("select next: cmp not visible: complete")
           cmp.complete()
         else
-          local info = debug.getinfo(fallback)
-          local msg = string.format("cmp: fallback to %s:%d", info.source, info.linedefined)
-          vim.notify(msg)
+          vim.notify("select_next: cmp not visible: fallback")
           fallback()
         end
       end, { "i", "s" }),
 
-      select_prev = cmp.mapping(function(fallback)
+      select_prev_item = cmp.mapping(function(fallback)
         if cmp.visible() then
+          vim.notify("select_prev: cmp visible: select_prev_item")
           cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
         else
+          vim.notify("select_prev: cmp not visible: fallback")
           fallback()
         end
       end, { "i", "s" }),
+
+      comfirm_or_next_placeholder = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          vim.notify("next paceholder: confirm")
+          cmp.confirm({ select = true })
+        elseif vim.snippet.active({ direction = 1 }) then
+          vim.notify("next paceholder: jump")
+          vim.snippet.jump(1)
+        else
+          vim.notify("next paceholder: fallback")
+          fallback()
+        end
+      end, { "i", "s" }),
+
+      prev_placeholder = cmp.mapping(function(fallback)
+        if vim.snippet.active({ direction = -1 }) then
+          vim.notify("prev paceholder: jump")
+          vim.snippet.jump(-1)
+        else
+          vim.notify("prev paceholder: fallback")
+          fallback()
+        end
+      end, { "i", "s" }),
+
     }
 
-    ---@diagnostic disable-next-line: missing-fields
+    -- based on: https://www.reddit.com/r/neovim/comments/woih9n/comment/ikbd6iy
+    local types = require("cmp.types")
+    local function deprioritize_snippet(entry1, entry2)
+      if entry1:get_kind() == types.lsp.CompletionItemKind.Snippet then return false end
+      if entry2:get_kind() == types.lsp.CompletionItemKind.Snippet then return true end
+    end
+
     cmp.setup({
-      enabled = true,
-      ---@diagnostic disable-next-line: missing-fields
+      mapping = {
+        -- words completion
+        ["<C-n>"] = actions.complete_or_select_next_item,
+        ["<C-p>"] = actions.select_prev_item,
+        ["<Tab>"] = actions.comfirm_or_next_placeholder,
+        ["<S-Tab>"] = actions.prev_placeholder,
+      },
       completion = {
+        completeopt = "menu,menuone,noselect",
+        placeholder = true,
         autocomplete = false,
-        completeopt = "menu,menuone,preview,noselect",
       },
-      snippet = {
-        expand = function(args)
-          require('luasnip').lsp_expand(args.body)
-        end,
+      sources = {
+        { name = 'nvim_lsp_signature_help', priority = 4 },
+        { name = "nvim_lsp",                priority = 3 },
+        { name = 'luasnip',                 priority = 2 },
+        { name = "path",                    priority = 1 },
       },
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-      --  I would like the following keymapping for my completions:
-      --
-      --  - <Tab> starts the completion
-      --  - <C-n> next item
-      --  - <C-p> previous item
-      --  - <C-j> scroll down the documentation
-      --  - <C-k> scroll up the documentation
-      --  - <C-y> go to the next snippet placeholder
-      --  - <C-y>p go to the previous snippet placeholder
-      --
-      --  This setup should not interfere with the default keybinding
-      --  of the copilot and may work way better for me.
-      mapping = cmp.mapping.preset.insert({
-        -- ["<Tab>"] = actions.select_next,
-        -- ['<C-n>'] = actions.select_next,
-        ["<Tab>"] = actions.select_next,
-        ['<C-n>'] = actions.select_next,
-
-        ["<S-Tab>"] = actions.select_prev,
-        ['<C-p>'] = actions.select_prev,
-
-        ['<C-k>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-j>'] = cmp.mapping.scroll_docs(4),
-
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<cr>'] = cmp.mapping.confirm({ select = true }), -- pick a selected entry only
-      }),
-      sources = cmp.config.sources({
-        { name = 'luasnip' },
-        { name = 'nvim_lsp',  keyword_length = 2 },
-        { name = 'async_path' },
-      }, {
-        { name = 'buffer', keyword_length = 3 },
-      }),
+      view = { entries = { name = "native" } },
       -- the only working option having multiple sources
       preselect = cmp.PreselectMode.None,
-
-      ---@diagnostic disable-next-line: missing-fields
+      snippet = {
+        expand = function(args)
+          -- in this case snippet is already set to luasnip
+          vim.snippet.expand(args.body)
+        end,
+      },
       formatting = {
         fields = { "kind", "abbr", "menu" },
-        format = make_formatting_function(),
+        format = make_format_function(),
+        expandable_indicator = true,
       },
+      sorting = {
+        priority_weight = 10,
+        comparators = {
+          deprioritize_snippet,
+          cmp.config.compare.offset,
+          cmp.config.compare.exact,
+          cmp.config.compare.score,
+          function(entry1, entry2)
+            local _, entry1_under = entry1.completion_item.label:find "^_+"
+            local _, entry2_under = entry2.completion_item.label:find "^_+"
+            entry1_under = entry1_under or 0
+            entry2_under = entry2_under or 0
+            if entry1_under > entry2_under then
+              return false
+            elseif entry1_under < entry2_under then
+              return true
+            end
+          end,
+          cmp.config.compare.kind,
+          cmp.config.compare.sort_text,
+          cmp.config.compare.length,
+          cmp.config.compare.order,
+        },
+      },
+    })
+
+    -- complete search with words from this buffer
+    cmp.setup.cmdline('/', {
+      mapping = cmp.mapping.preset.cmdline(),
+      view = { entries = { name = "custom" } },
+      formatting = {
+        fields = { "abbr" },
+        format = make_format_function(),
+        expandable_indicator = true,
+      },
+      sources = { { name = 'buffer' } }
+    })
+    -- complete wim commands
+    cmp.setup.cmdline(':', {
+      mapping = cmp.mapping.preset.cmdline(),
+      view = { entries = { name = "custom" } },
+      formatting = {
+        fields = { "abbr" },
+        format = make_format_function(),
+        expandable_indicator = true,
+      },
+      window = {
+        completion = cmp.config.window.bordered({ border = 'none' }),
+      },
+      sources = cmp.config.sources(
+        { { name = 'path' } },
+        { { name = 'cmdline', option = { ignore_cmds = { 'Man', '!' } } } }
+      )
     })
   end
 }
