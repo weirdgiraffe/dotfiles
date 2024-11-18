c1f5f505873fc9f40521efbb345255c6bf2247d1
weirdgiraffe <giraffe@cyberzoo.xyz>
2024-11-18 10:01:30 +0100

    nvim: fixup cmp-nvim cmdline completions

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -32,6 +32,7 @@ return {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-cmdline",
     "hrsh7th/cmp-nvim-lsp-signature-help",
     "onsails/lspkind.nvim",
     "L3MON4D3/LuaSnip",
