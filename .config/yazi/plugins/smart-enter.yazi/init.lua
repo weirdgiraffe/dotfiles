0b967df5b28ee92acfa33086cc9624b90d7aab58
weirdgiraffe <giraffe@cyberzoo.xyz>
2025-01-08 15:41:12 +0100

    fixup: update yazi keybindings after the update

--- .config/yazi/plugins/smart-enter.yazi/init.lua
+++ .config/yazi/plugins/smart-enter.yazi/init.lua
@@ -1,9 +1,10 @@
--- plugin to enter directory or open file on l
--- https://yazi-rs.github.io/docs/faq/#why-cant-open-and-enter-be-a-single-command
--- https://yazi-rs.github.io/docs/tips/#smart-enter-enter-for-directory-open-for-file
-return {
-  entry = function()
-    local h = cx.active.current.hovered
-    ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = true })
-  end,
-}
+--- @sync entry
+
+local function setup(self, opts) self.open_multi = opts.open_multi end
+
+local function entry(self)
+  local h = cx.active.current.hovered
+  ya.manager_emit(h and h.cha.is_dir and "enter" or "open", { hovered = not self.open_multi })
+end
+
+return { entry = entry, setup = setup }
