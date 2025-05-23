a2324dd6a9fc0ff87c12054be4cf72c820b8c96c
weirdgiraffe <giraffe@cyberzoo.xyz>
2025-05-23 11:29:13 +0200

    chore: update to current state

--- .config/nvim/lua/plugins/markdown-preview.lua
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -1,5 +1,17 @@
 return {
   "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
-  build = function() vim.fn["mkdp#util#install"]() end,
+  build = function(plugin)
+    if vim.fn.executable "npx" then
+      vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install")
+    else
+      vim.cmd [[Lazy load markdown-preview.nvim]]
+      vim.fn["mkdp#util#install"]()
+    end
+  end,
+  init = function()
+    vim.g.mkdp_theme = "dark"
+    if vim.fn.executable "npx" then vim.g.mkdp_filetypes = { "markdown" } end
+  end,
 }
