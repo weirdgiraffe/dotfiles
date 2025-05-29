d2593d95642cb46b2b88361f5db24558a88596e3
weirdgiraffe <giraffe@cyberzoo.xyz>
2025-05-29 15:50:15 +0200

    update: stdpaths

--- .config/nvim/lua/config/stdpath.lua
+++ .config/nvim/lua/config/stdpath.lua
@@ -1,15 +1,6 @@
---[[
 return {
   state  = vim.fn.stdpath("state"),
   data   = vim.fn.stdpath("data"),
   config = vim.fn.stdpath("config"),
-  log = vim.fn.stdpath("log"),
-}
---]]
-return {
-  state  = "/Users/viktor/nvim-raise/state",
-  data   = "/Users/viktor/nvim-raise/data",
-  config = "/Users/viktor/nvim-raise/nvim",
-  cache  = "/Users/viktor/nvim-raise/cache",
-  log    = "/Users/viktor/nvim-raise/state/nvim",
+  log    = vim.fn.stdpath("log"),
 }
