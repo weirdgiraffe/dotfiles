a01862075c0f47995ee0c7af5ebe4a604b7af0d2
weirdgiraffe <giraffe@cyberzoo.xyz>
2025-07-28 11:55:32 +0200

    add comment plugin

---
+++ .config/nvim-simple/lua/plugins/comment.lua
@@ -0,0 +1,5 @@
+return {
+  "numToStr/comment.nvim",
+  lazy = false,
+  opts = {}
+}
--- .config/nvim-simple/lua/plugins/lualine.lua
+++ .config/nvim-simple/lua/plugins/lualine.lua
@@ -8,60 +8,58 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons"
   },
-  config = function()
-    require('lualine').setup({
-      options = {
-        theme = theme,
-        icons_enabled = true,
-        component_separators = "",
-        -- section_separators = { left = '', right = '' },
-        section_separators = "",
+  opts = {
+    options = {
+      theme = theme,
+      icons_enabled = true,
+      component_separators = "",
+      -- section_separators = { left = '', right = '' },
+      section_separators = "",
+    },
+    -- in the tabline I would like to show the list of open buffers
+    -- at the same time I would like to show buffer numbers for the
+    -- open buffers and enable switch between open buffers using
+    -- leader + number keyboard shortcut
+    tabline = {
+      lualine_a = {
+        { "buffers", mode = 2, },
       },
-      -- in the tabline I would like to show the list of open buffers
-      -- at the same time I would like to show buffer numbers for the
-      -- open buffers and enable switch between open buffers using
-      -- leader + number keyboard shortcut
-      tabline = {
-        lualine_a = {
-          { "buffers", mode = 2, },
-        },
-        lualine_b = {},
-        lualine_c = {},
-        lualine_x = {},
-        lualine_y = {},
-        lualine_z = {},
-      },
-      sections = {
-        lualine_a = {
-          {
-            function() return ' ' end,
-            padding = { left = 1, right = 1 },
-          },
-        },
-        lualine_b = {
-          'branch',
-          'diff',
-          {
-            'diagnostics',
-            sources = { 'nvim_workspace_diagnostic' },
-            sections = { 'error', 'warn' },
-            colored = true,
-            update_in_insert = false,
-            symbols = { error = " ", warn = " " },
-          },
-        },
-        lualine_c = {
-          { 'filename', path = 3 },
-        },
-        lualine_x = {},
-        lualine_y = { 'encoding', 'fileformat', 'filetype' },
-        -- lualine_z = { 'location' },
-        lualine_z = {
-          { 'selectioncount', color = 'Search' },
-          { 'progress' },
-          { 'location' },
+      lualine_b = {},
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {},
+    },
+    sections = {
+      lualine_a = {
+        {
+          function() return ' ' end,
+          padding = { left = 1, right = 1 },
         },
       },
-    })
-  end,
+      lualine_b = {
+        'branch',
+        'diff',
+        {
+          'diagnostics',
+          sources = { 'nvim_workspace_diagnostic' },
+          sections = { 'error', 'warn' },
+          colored = true,
+          update_in_insert = false,
+          symbols = { error = " ", warn = " " },
+        },
+      },
+      lualine_c = {
+        { 'filename', path = 3 },
+      },
+      lualine_x = {},
+      lualine_y = { 'encoding', 'fileformat', 'filetype' },
+      -- lualine_z = { 'location' },
+      lualine_z = {
+        { 'selectioncount', color = 'Search' },
+        { 'progress' },
+        { 'location' },
+      },
+    },
+  },
 }
