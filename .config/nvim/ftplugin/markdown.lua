5ec9f13c7c598b889eb6ff17a845d980e7ab464f
weirdgiraffe <giraffe@cyberzoo.xyz>
2025-08-04 08:57:30 +0200

    feat(nvim): do not wrap lines in markdown

--- .config/nvim/ftplugin/markdown.lua
+++ .config/nvim/ftplugin/markdown.lua
@@ -1,6 +1,16 @@
-vim.o.textwidth = 120
-vim.o.colorcolumn = "+1"
-vim.cmd [[ hi ColorColumn ctermbg=blue guibg=blue ]]
+-- soft wrap long lines in markdown
+-- vim.o.textwidth = 120
+-- vim.o.colorcolumn = "+1"
+-- vim.cmd [[ hi ColorColumn ctermbg=grey guibg=grey ]]
+
+-- soft wrap long lines in markdown
+vim.o.wrap = true
+vim.o.linebreak = true
+vim.o.showbreak = " "
+vim.o.colorcolumn = "110"
+vim.o.textwidth = 0  -- disable hard wrapping
+vim.o.wrapmargin = 0 -- disable wrap margin
+vim.o.list = false   -- disable whitespaces display
 
 vim.o.tabstop = 8
 vim.o.softtabstop = 2
