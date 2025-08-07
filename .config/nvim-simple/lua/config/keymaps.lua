768439932529e1a60cf79118a11c4c4fbfe12785
weirdgiraffe <giraffe@cyberzoo.xyz>
2025-08-07 15:24:28 +0200

    fixup: gitsigns keys

--- .config/nvim-simple/lua/config/keymaps.lua
+++ .config/nvim-simple/lua/config/keymaps.lua
@@ -21,3 +21,61 @@ for i = 1, 9, 1 do
     require("utils.buffers").switch_to_buffer(i)
   end, "switch to buffer " .. i)
 end
+
+
+
+local M = {
+}
+
+function M.gitsigns_on_attach(bufnr)
+  local gs = require("gitsigns")
+  local wk = require("which-key")
+  local function map(mode, lhs, desc, rhs, opts)
+    local spec = { lhs, rhs }
+    for k, v in pairs(opts or {}) do spec[k] = v end
+    spec.mode = mode
+    spec.desc = desc
+    spec.buffer = bufnr
+    wk.add(spec)
+  end
+
+  -- Navigation
+  map('n', ']c', 'Go to next hunk', function()
+    if vim.wo.diff then
+      vim.cmd.normal({ '[c', bang = true })
+    else
+      gs.nav_hunk('next')
+    end
+  end, { expr = true })
+
+  map('n', '[c', 'Go to next hunk', function()
+    if vim.wo.diff then
+      vim.cmd.normal({ '[c', bang = true })
+    else
+      gs.nav_hunk('prev')
+    end
+  end, { expr = true })
+
+  -- Actions
+  map('n', '<leader>hs', "Stage hunk", function() gs.stage_hunk() end)
+  map('n', '<leader>hu', "Undo stage hunk", function() gs.stage_hunk() end)
+  map('n', '<leader>hr', "Reset hunk", function() gs.reset_hunk() end)
+  map('v', '<leader>hs', "Stage hunk", function() gs.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end)
+
+
+
+  map('v', '<leader>hr', "Reset hunk", function() gs.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') }) end)
+  map('n', '<leader>hS', "Stage buffer", function() gs.stage_buffer() end)
+  map('n', '<leader>hR', "Reset buffer", function() gs.reset_buffer() end)
+  map('n', '<leader>hp', "Preview hunk", function() gs.preview_hunk() end)
+  map('n', '<leader>hb', "Blame line", function() gs.blame_line({ full = true }) end)
+  map('n', '<leader>tb', "Toggle current line blame", function() gs.toggle_current_line_blame() end)
+  map('n', '<leader>hd', "Diff this", function() gs.diffthis() end)
+  map('n', '<leader>hD', "Diff this file", function() gs.diffthis() end)
+  map('n', '<leader>td', "Toggle deleted", function() gs.preview_hunk_inline() end)
+
+  -- Text object
+  map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', "Select hunk")
+end
+
+return M
