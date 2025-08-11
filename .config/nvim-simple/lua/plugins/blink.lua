c3704a8ff9648e583d558514e559d169e0c35808
weirdgiraffe <giraffe@cyberzoo.xyz>
2025-08-11 10:36:53 +0200

    feat: configure completions

--- .config/nvim-simple/lua/plugins/blink.lua
+++ .config/nvim-simple/lua/plugins/blink.lua
@@ -1,3 +1,15 @@
+--- Remove all matches from items for which filter returns true
+---@param items any[] items to process
+---@param filter fun(item:any):boolean filter to apply
+local function filter_out(items, filter)
+  for i = #items, 1, -1 do
+    if filter(items[i]) then
+      table.remove(items, i)
+    end
+  end
+  return items
+end
+
 return {
   'saghen/blink.cmp',
   version = '1.*',
@@ -9,21 +21,29 @@ return {
   opts = {
     keymap = {
       preset = 'none',
-      ["<C-n>"] = { -- show completions or move to the next one
-        'show',
-        'select_next',
-        'fallback',
-      },
-      ["<C-p>"] = { -- show completions or move to the next one
-        'select_prev',
-        'fallback',
-      },
-      ["<C-l>"] = { -- accept the completion
-        'accept',
-        'fallback'
-      },
-      ["<CR>"] = { -- accept the completion
-        'accept',
+      ["<C-n>"] = { 'show', 'select_next', 'fallback', },
+      ["<C-p>"] = { 'show', 'select_prev', 'fallback', },
+      ['<C-k>'] = { 'scroll_documentation_up', 'fallback' },
+      ['<C-j>'] = { 'scroll_documentation_down', 'fallback' },
+      ["<CR>"] = { 'accept', 'fallback' },
+      ["<Tab>"] = {
+        function(cmp)
+          -- next snippet item if we are inside of the snippet
+          if cmp.snippet_active({ direction = 1 }) then
+            return cmp.snippet_forward()
+          end
+
+          -- completion may be active because of the trigger character,
+          if cmp.is_active() then
+            local items = cmp.get_items()
+            if cmp.is_visible() then
+              return #items == 1 and cmp.accept() or cmp.select_next()
+            end
+            return cmp.show_and_insert()
+          end
+
+          return cmp.is_visible() or cmp.show({ providers = { "snippets" } })
+        end,
         'fallback'
       },
     },
@@ -34,13 +54,16 @@ return {
     completion = {
       trigger = {
         show_on_insert = false,
-        show_on_keyword = false,
-        show_on_trigger_character = false,
+        show_on_keyword = true,
+        show_on_trigger_character = true,
       },
       accept = { auto_brackets = { enabled = false } },
-      list = { selection = { preselect = false, auto_insert = true } },
+      list = { selection = { preselect = false, auto_insert = false } },
       ghost_text = { enabled = true },
-      documentation = { auto_show = true },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+      },
       menu = {
         auto_show = false,
         draw = {
@@ -52,7 +75,13 @@ return {
       },
     },
     sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
+      default = { "lsp", "path", "buffer" },
+      transform_items = function(_, items)
+        local kind = require("blink.cmp.types").CompletionItemKind
+        return filter_out(items, function(item)
+          return item.source_name == "LSP" and item.client_name == "gopls" and item.kind == kind.Snippet
+        end)
+      end,
     },
     -- Use a preset for snippets, check the snippets documentation for more information
     snippets = { preset = 'luasnip' },
