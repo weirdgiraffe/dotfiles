fa6e744ba27f01c84e5819e1b2f60ce66563d0f1
weirdgiraffe <giraffe@cyberzoo.xyz>
2025-08-14 08:54:31 +0200

    feat(nvim): use claude for minuet

--- .config/nvim/lua/plugins/minuet-ai.lua
+++ .config/nvim/lua/plugins/minuet-ai.lua
@@ -7,7 +7,7 @@ return {
     require("minuet").setup({
       cmp = { enable_auto_complete = false },
       blink = { enable_auto_complete = true },
-      provider = 'codestral',
+      provider = 'claude',
       notify = 'warn',
       provider_options = {
         codestral = {
@@ -18,6 +18,17 @@ return {
           optional = {
             max_tokens = 256,
             stop = { '\n\n' },
+          },
+        },
+        claude = {
+          max_tokens = 512,
+          model = 'claude-3-5-haiku-20241022',
+          stream = true,
+          api_key = 'ANTHROPIC_API_KEY',
+          end_point = 'https://api.anthropic.com/v1/messages',
+          optional = {
+            max_tokens = 256,
+            top_p = 0.9,
           },
         },
       },
@@ -36,9 +47,10 @@ return {
       -- Make the ghost text disappear quickly if it doesn't match what you type
       after_cursor_filter_length = 15,
       before_cursor_filter_length = 1,
-      debounce = 400,
-      throttle = 800,
-      request_timeout = 3,
+      -- Make requests infrequently
+      request_timeout = 3.0, -- seconds
+      throttle = 3000,       -- ms; minimum gap between requests (increase if needed)
+      debounce = 600,        -- ms; wait after you stop typing before sending
     })
   end
 }
